acbbf9f886d3cf17c82d5596aefb24e040c0ad97
smeetagrawal <smeetagrawal>
2019-09-09 02:08:28 +0530

    implementation of third page comment scraper

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio');
 let browser;
 let page;
 
-(async function main(){
+(async function main() {
 
     try {
 
@@ -25,9 +25,9 @@ let page;
         let firstPageNavigationUrl
 
         let length = $('.wdH28 > div').find('div._1GdgP').length
-        console.log("smeet",length)
+        console.log("smeet", length)
 
-        for(i = 0; i < length; i++){
+        for (i = 0; i < length; i++) {
 
             await page.goto('https://experts.shopify.com/services/expert-guidance');
             await page.waitForSelector('.Zk7Cl');
@@ -38,7 +38,7 @@ let page;
 
             let section = $('.wdH28 > div').eq(i).html()
             // console.log("section ",section)
-            console.log('i',i)
+            console.log('i', i)
 
 
             let $2 = cheerio.load(section);
@@ -48,166 +48,214 @@ let page;
             let firstPageDescription = $2('._3igjR > ._1c52H > .Polaris-TextContainer > p')
 
             console.log("first page image urls", firstPageImageUrls);
-            console.log("first page heading",firstPageHeadings.text());
-            console.log("first page Description",firstPageDescription.text());
-            console.log("first page navigation urls",firstPageNavigationUrl);
+            console.log("first page heading", firstPageHeadings.text());
+            console.log("first page Description", firstPageDescription.text());
+            console.log("first page navigation urls", firstPageNavigationUrl);
             console.log('\n')
 
             await page.goto(firstPageNavigationUrl, { waitUntil: 'networkidle0' });
             await page.waitForSelector('._3EE3N');
 
-            let result = await secondPageFunction();
-            console.log("this is result",result);
+            let result = await checkSecondPage(firstPageNavigationUrl);
+            console.log("this is result", result);
 
             //    break;
         }
 
-    } catch(error) {
-        console.log("this is dsfdsd error",error)
+    } catch (error) {
+        console.log("this is dsfdsd error", error)
     }
-    
+
 
 })()
 
+function checkSecondPage(secondPageUrl) {
+    let page_no = 1;
+    // let url = `${secondPageUrl}?page=${page_no}`;
+    let status = true;
+    return new Promise(async (resolve, reject) => {
+        while(status){
 
-function secondPageFunction(){
-    
-    return new Promise(async (resolve,reject) => {
+            console.log("this is while loop ",page_no)
+            await page.goto(`${secondPageUrl}?page=${page_no}`, { waitUntil: 'networkidle0' });
+            await page.waitForSelector('._3EE3N');
+
+            let $secondPage = cheerio.load(await page.evaluate(() => document.body.innerHTML));
+
+            //  this is the root of second page
+            let secondPageRoot = cheerio.load($secondPage('div.Zk7Cl > div:nth-child(2)').html());
+            // this is the parent of second page
+            let secondPageParent = cheerio.load(secondPageRoot('section.jGIW4 > div:nth-child(2)').html());
+
+            let secondPageLength = secondPageParent('body').find('div.kVWrj').length
+
+            // console.log("this is smeet html",secondPageLength);
+
+            if(secondPageLength === 0){  // this will check if current page has data than only we proceed else we will resolve here
+                // console.log("here we will terminate and resolve");
+                status = false;
+                resolve();
+            } else {
+                let result = await extractSecondPage();
+                if(result){
+                    page_no += 1;
+                }
+               
+            }
+
+        }
+        
+        
+    })
+}
+
+function extractSecondPage() {
+
+    return new Promise(async (resolve, reject) => {
 
         try {
 
-        /*------------------------------------Second Page Coding starts from here-----------------------------------------------------------------------------*/      
+            /*------------------------------------Second Page Coding starts from here-----------------------------------------------------------------------------*/
 
-        // this will load second page html, which is fetched with help of puppeeter
-        let secondPageBodyHtml = await page.evaluate(() => document.body.innerHTML);
-        // this will load all the html with the help of cheerio 
-        let $secondPage = cheerio.load(secondPageBodyHtml);
+            // this will load second page html, which is fetched with help of puppeeter
+            let secondPageBodyHtml = await page.evaluate(() => document.body.innerHTML);
+            // this will load all the html with the help of cheerio 
+            let $secondPage = cheerio.load(secondPageBodyHtml);
 
-        //  this is the root of second page
-        let secondPageRoot = $secondPage('div.Zk7Cl > div:nth-child(2)').html();
+            //  this is the root of second page
+            let secondPageRoot = $secondPage('div.Zk7Cl > div:nth-child(2)').html();
 
-        console.log("this is html data",$secondPage.html())
-        let $secondPageP = cheerio.load(secondPageRoot);
-        // this is the parent of second page
-        let secondPageParent = $secondPageP('section.jGIW4 > div:nth-child(2)').html();
+            // console.log("this is html data", $secondPage.html())
+            let $secondPageP = cheerio.load(secondPageRoot);
+            // this is the parent of second page
+            let secondPageParent = $secondPageP('section.jGIW4 > div:nth-child(2)').html();
 
-        let $secondPagep1 = cheerio.load(secondPageParent)
+            let $secondPagep1 = cheerio.load(secondPageParent)
 
-        let secondPageLength = $secondPagep1('body').find('div.kVWrj').length
+            let secondPageLength = $secondPagep1('body').find('div.kVWrj').length
 
-        console.log("this is lenght",secondPageLength);
-
-        
-        for(let j = 0; j < secondPageLength; j++){
-
-            console.log("this is second page loop",j)
-            
-            let div = $secondPagep1('body > div.kVWrj').eq(j).html();
-
-            let $SecondPage = cheerio.load(div)
-            // console.log("this is single div data",$SecondPage.html())
-
-            let secondPageCardChild1 = $SecondPage('._2nijJ').html();
-
-            let secondPageCardChild2 = $SecondPage('._3UNHw').html();
-
-            let $secondPageCardChild1 = cheerio.load(secondPageCardChild1);
-            let $secondPageCardChild2 = cheerio.load(secondPageCardChild2);
-
-            
-
-            let secondPageChidl1 = cheerio.load($secondPageCardChild2('div._3f0Kq > div:nth-child(1)').html());
-            let secondPageChidl2 = cheerio.load($secondPageCardChild2('div._3f0Kq > div:nth-child(2)').html());
-            let secondPageChild3 = cheerio.load($secondPageCardChild2('div._3f0Kq > div:nth-child(3)').html());
-
-           let secondPageCompanyLogo = $secondPageCardChild1('._1WR9f._1tJOR > span > img').attr('src');
-           let secondPageCompanyName = $secondPageCardChild1('div._2dskn > h2').html();
-           let secondPageCompanyLocation = $secondPageCardChild1('div._2dskn > div > .Polaris-TextStyle--variationSubdued').html();
-           let secondPageStartingPrice = secondPageChidl1('div._3657d').html();
-           let secondPageJobsCompleted = secondPageChidl2('div >div >div:nth-child(2)').html();
-           let secondPageRating = secondPageChild3('div > div > div:nth-child(2) > div > div:nth-child(1)').html();
+            console.log("this is length", secondPageLength);
 
 
-            console.log("this is second page logo",secondPageCompanyLogo);
-            console.log("this is second page Company Name",secondPageCompanyName);
-            console.log("this is second page Company Location",secondPageCompanyLocation);
-            console.log("this is second page price",secondPageStartingPrice);
-            console.log("this is second page jobs completed",secondPageJobsCompleted);
-            console.log("this is second page ratings",secondPageRating);
+            for (let j = 0; j < secondPageLength; j++) {
+
+                console.log("this is second page loop", j)
+
+                let div = $secondPagep1('body > div.kVWrj').eq(j).html();
+
+                let $SecondPage = cheerio.load(div)
+                // console.log("this is single div data",$SecondPage.html())
+
+                let secondPageCardChild1 = $SecondPage('._2nijJ').html();
+
+                let secondPageCardChild2 = $SecondPage('._3UNHw').html();
+
+                let $secondPageCardChild1 = cheerio.load(secondPageCardChild1);
+                let $secondPageCardChild2 = cheerio.load(secondPageCardChild2);
 
 
-            console.log('/n');
+
+                let secondPageChidl1 = cheerio.load($secondPageCardChild2('div._3f0Kq > div:nth-child(1)').html());
+                let secondPageChidl2 = cheerio.load($secondPageCardChild2('div._3f0Kq > div:nth-child(2)').html());
+                let secondPageChild3 = cheerio.load($secondPageCardChild2('div._3f0Kq > div:nth-child(3)').html());
+
+                let secondPageCompanyLogo = $secondPageCardChild1('._1WR9f._1tJOR > span > img').attr('src');
+                let secondPageCompanyName = $secondPageCardChild1('div._2dskn > h2').html();
+                let secondPageCompanyLocation = $secondPageCardChild1('div._2dskn > div > .Polaris-TextStyle--variationSubdued').html();
+                let secondPageStartingPrice = secondPageChidl1('div._3657d').html();
+                let secondPageJobsCompleted = secondPageChidl2('div >div >div:nth-child(2)').html();
+                let secondPageRating = secondPageChild3('div > div > div:nth-child(2) > div > div:nth-child(1)').html();
 
 
-            let thirdPageUrl = `https://experts.shopify.com${$secondPagep1('body > div.kVWrj > a').eq(j).attr('href')}`;
-            console.log('this is third page url************************',thirdPageUrl);
-            await thirdPage(thirdPageUrl);
-            if((secondPageLength - 1) === j){
-                resolve('second page loop is completed');
+                console.log("this is second page logo", secondPageCompanyLogo);
+                console.log("this is second page Company Name", secondPageCompanyName);
+                console.log("this is second page Company Location", secondPageCompanyLocation);
+                console.log("this is second page price", secondPageStartingPrice);
+                console.log("this is second page jobs completed", secondPageJobsCompleted);
+                console.log("this is second page ratings", secondPageRating);
+
+
+                console.log('/n');
+
+
+                let thirdPageUrl = `https://experts.shopify.com${$secondPagep1('body > div.kVWrj > a').eq(j).attr('href')}`;
+                console.log('this is third page url************************', thirdPageUrl);
+                await extractThirdPage(thirdPageUrl);
+                if ((secondPageLength - 1) === j) {
+                    resolve(true);
+                }
+                // break;
+
             }
-            // break;
-
-        }
 
 
-        } catch(error) {
-            console.log("error",error);
+        } catch (error) {
+            console.log("error", error);
             reject(error)
         }
     })
 }
 
 
-function thirdPage(thirdPageUrl){
-    return new Promise(async (resolve,reject) => {
+function extractThirdPage(thirdPageUrl) {
+    return new Promise(async (resolve, reject) => {
 
-        await page.goto(thirdPageUrl);
+        await page.goto(thirdPageUrl, { waitUntil: 'networkidle0' });
         await page.waitForSelector('._34cm1');
-        let headerData;
-        let bodyData;
+        // this will check if comments present or not (single comment)
+        let thirdPageParent = cheerio.load( await page.evaluate(() => document.body.innerHTML) )
+        let thirdPageRoot = cheerio.load(thirdPageParent('div.u7SZF').html());
+        let length =  thirdPageRoot('div._34cm1').find('._2RRwD').length;
+        console.log("length check before scrapping comments",length);
+        if(length === 0){ // check if single comment is present, if not it will resolve 
+            resolve();
+        } else {
+            let result = await extractComments();
+            if(result){
+                resolve();
+            }
+        }
 
-        let result = await response(headerData,bodyData);
-        resolve()
-        // console.log("this is promise result",result.body.data.profile.serviceListing.reviews.edges);
+        
     })
-    
+
 }
 
-function response(headerData, bodyData){
-    return new Promise((resolve,reject) => {
 
-        try {
-
-            page.on('response',async (response) => {
-
-                if (response.url().endsWith("api/graphql")){
-                    console.log("this is response url",response.url());
-                    let result = await response.json();
-                    console.log("this is all result",result);
-                    if(result.data.hasOwnProperty('profile')){
-                        if(result.data.profile.hasOwnProperty('serviceListing')){
-    
-                            if(Object.keys(result.data.profile).length > 2){
-                                headerData = result;
-                            } else {
-                                bodyData = result;
-                                resolve({ body:bodyData, header: headerData });
-                            }
-                        }
-                    }
-                    
-                        
-                }
-        
-            })
-
-        } catch(error){
-            console.log("this is error",error);
+function extractComments() {
+    return new Promise(async (resolve, reject) => {
+        let status = true;
+        while(status){
+            
+            
+            let thirdPageParent = cheerio.load( await page.evaluate(() => document.body.innerHTML) )
+            let thirdPageRoot = cheerio.load(thirdPageParent('div.u7SZF').html());
+            let length =  thirdPageRoot('div._34cm1').find('._2RRwD').length;
+            // let props = thirdPageRoot('div._34cm1 > nav > button:nth-child(2)').prop('disabled');
+            let element = await page.$('div._34cm1 > nav > button:nth-child(2)');
+            let props =  await ( await element.getProperty('disabled') ).jsonValue(); 
+            console.log("this is props",props);
+            // for(let i = 0; i < length; i++){
+            //     let article = cheerio.load( thirdPageRoot('div._34cm1 > ._2RRwD').eq(i).html() );
+            //     console.log("this is article",i,article.html());
+            // }
+            
+            if(props){
+                console.log("breaking it",length);
+                resolve(true);
+                break;  
+            } else {
+                console.log("this is total length",length);
+                await Promise.all([
+                    page.click('div._34cm1 > nav > button:nth-child(2)'),
+                    page.waitFor(2000)
+                ]);
+            }
+           
         }
-        
 
     })
 
-    
+
 
 }
